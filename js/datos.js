3f6b3583832b26ccd8934aba73e8e9035bfba967
Elena Diaz Garrido <elena.diazgarrido@gmail.com>
2021-01-31 12:19:08 +0100

    array creado

---
+++ js/datos.js
@@ -0,0 +1,20 @@
+let listaTareas = new Array();
+
+listaTareas = new Array (
+    {
+        idTarea: 1,
+        titulo: 'Tarea de ejemplo numero uno',
+        prioridad: 'Baja',
+    },
+    {
+        idTarea: 2,
+        titulo: 'Tarea de ejemplo numero dos',
+        prioridad: 'Media'
+    },
+    {
+        idTarea: 3,
+        titulo: 'Tarea de ejemplo numero tres',
+        prioridad: 'Alta',
+    },
+)
+
