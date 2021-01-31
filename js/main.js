e2b422903bffe7046dc71b08136966dd49e430ea
Elena Diaz Garrido <elena.diazgarrido@gmail.com>
2021-01-31 20:37:04 +0100

    delete tarea terminado

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const sectionTasks = document.querySelector('#lista')
+const inputTitle = document.querySelector('#titulo');
+const inputPriority = document.querySelector('#prioridad');
 
 //----- PRINT TASK
 const printAllTasks = function (pTaskList) {
@@ -7,6 +9,7 @@ const printAllTasks = function (pTaskList) {
         printOneTask(task);
     }
 }
+
 
 const printOneTask = function (pTask) {
 
@@ -19,19 +22,19 @@ const printOneTask = function (pTask) {
     button.appendChild(contentButton);
     li.appendChild(button)
 
+    li.classList.add('tarea2');
     button.classList.add('delete');
     button.dataset.id = pTask.id;
-    /* button.addEventListener('click', deleteTask); */
+    button.addEventListener('click', deleteTask);
 
     sectionTasks.appendChild(li)
+
 }
 
 printAllTasks(listaTareas);
 
 //----- ADD TASK FROM FORM
 
-const inputTitle = document.querySelector('#titulo');
-const inputPriority = document.querySelector('#prioridad');
 
 let idTask = listaTareas.length
 
@@ -40,35 +43,71 @@ const btnForm = document.querySelector('#btn');
 btnForm.addEventListener('click', getDataForm)
 
 function getDataForm(event) {
-    event.preventDefault(); 
+    event.preventDefault();
+
+    
 
     const title = inputTitle.value;
     const priority = inputPriority.value;
 
+
     if (title != "" && priority != "") {
         const newTask = {
-            id: ++idTask, 
+            id: ++idTask,
             titulo: title,
             prioridad: priority,
         };
         saveTask(newTask);
     } else {
         alert('Completa todos los campos');
-    }
+    } 
 }
 
-function saveTask (pTask){
+function saveTask(pTask) {
     listaTareas.push(pTask);
     printOneTask(pTask);
     console.log(listaTareas);
 }
 
+//-------- DELETE TASK
+
+function deleteTask(event) {
+
+    let li = event.target.parentNode;
+    li.parentNode.removeChild(li)
+
+    let id = parseInt(event.target.dataset.id);
+    let position = listaTareas.findIndex(task => task.id === id);
+
+    listaTareas.splice(position, 1)
+    console.log(listaTareas);
+
+}
+
+
+//----- FILTER TASK BY PRIORITY
+
+const btnFilter = 
 
 
 
 
-//----------Funciones
+
+
+/*
+
+cambiar color del li como cojones lo hago!!!
+
+let li = document.querySelector('.tarea')
 
 
 
-//----------Eventos
+if (priority == 'Baja') {
+    li.style.backgroundColor = 'black'
+} else if (priority == 'Media') {
+    li.style.backgroundColor = 'white'
+} else if (priority == 'Alta') {
+    li.style.backgroundColor = 'tomato'
+} else {
+    alert('Completa todos los campos');
+} */
